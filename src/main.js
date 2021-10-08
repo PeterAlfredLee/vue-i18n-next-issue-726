92502a1526610b31ad378309fba57b7a6f37b7c1
PeterAlfredLee <peteralfredlee@gmail.com>
2021-10-08 10:11:35 +0800

    try to reproduce vue-i18n-next issue #726

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
+import en from './locales/en.json'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  globalInjection: true,
+  legacy: false,
+  locale: 'en',
+  messages: {
+    en
+  }
+})
+
+createApp(App).use(i18n).mount('#app')
